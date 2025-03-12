42339f65a7df9e3d32ae44cd7be9e1ba2ae15237
Christhudhas <Merlindas941@gmail.com>
2025-03-13 02:46:44 +0530

    agni

---
+++ comment.java
@@ -0,0 +1,10 @@
+public class comment {
+    /*
+     1)demo
+     2)using variable
+     */
+    public static void main (String args[]){
+        int num1=20;
+        System.out.println(num1);
+    }
+}
