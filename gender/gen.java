a9d00cca68dd151c61b0573a8225828458116e15
christhudhas941 <merlindas941@gmail.com>
2025-03-13 12:08:08 +0530

    gender

---
+++ gender/gen.java
@@ -0,0 +1,13 @@
+public class gen {
+    public static void main(String args[])
+    {
+        String gender="Male";
+        if(true){
+            System.out.println("It is positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+    
+}
