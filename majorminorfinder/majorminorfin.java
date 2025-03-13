5cbb60852b89f9fa7ff6865ba5c0f30d345784bb
christhudhas941 <merlindas941@gmail.com>
2025-03-13 11:18:02 +0530

    age

---
+++ majorminorfinder/majorminorfin.java
@@ -0,0 +1,16 @@
+public class majorminorfin {
+    public static void main(String args[]){
+        
+        int age=21;
+
+    
+        if(age>18){
+        System.out.println("minor");
+    }
+    else
+    {
+        System.out.println("major");
+
+    }
+}
+}
