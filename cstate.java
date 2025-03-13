33370551570a67f9fb256db1ac9770363d0e2145
christhudhas941 <merlindas941@gmail.com>
2025-03-13 10:48:29 +0530

    if statement

---
+++ cstate.java
@@ -0,0 +1,12 @@
+public class cstate {
+    public static void main(String[] args) {
+        if (true) {
+        System.out.println("Their will be rain tommorrow");
+        }
+        if (false) 
+        {
+        System.out.println("enjoy sunlight");    
+        }
+    }
+    
+}
