f0e58cf4bd2d0858ac5ed624aeb7d0ef2b180532
christhudhas941 <merlindas941@gmail.com>
2025-03-13 14:59:19 +0530

    m

---
+++ Scanner/scan.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class scan {
+
+    public static void main(String args[])
+    {
+            Scanner scan=new Scanner(System.in);
+            String name=scan.nextLine();
+            System.out.println(name);
+    }
+}
