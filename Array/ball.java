6bf74c82eadfc38cb39e1c9598845e5f432454cc
christhudhas941 <merlindas941@gmail.com>
2025-03-13 13:26:39 +0530

    array2

---
+++ Array/ball.java
@@ -0,0 +1,9 @@
+public class ball {
+    public static void main(String[] args) 
+    {
+        String balls[]={"red","blue","yellow"};
+        balls[0]="red";
+        System.out.println(balls[0]);
+    }
+    
+}
