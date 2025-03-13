efe3bf0e1109d3c33d88c8a62efe72d4c52ab863
christhudhas941 <merlindas941@gmail.com>
2025-03-13 10:26:13 +0530

    arithmetic

---
+++ arithmetic.java
@@ -0,0 +1,9 @@
+public class arithmetic {
+    public static void main(String args[])
+    {
+        System.out.println(10+2);
+        System.out.println(20-98);
+        System.out.println(5*8);
+    }
+    
+}
