f1b284f7d53e2b2ab00dcb829e0c311d4d7a353a
christhudhas941 <merlindas941@gmail.com>
2025-03-13 10:31:44 +0530

    conditional

---
+++ condition.java
@@ -0,0 +1,11 @@
+public class condition{
+    public static void main(String args[])
+    {
+        System.out.println(23<45);
+        System.out.println(88>45);
+        System.out.println(76>=43);
+        System.out.println(45<=56);
+        System.out.println(3==3);
+        System.out.println(5!=6);
+    }
+}
