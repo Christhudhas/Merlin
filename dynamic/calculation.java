dbbf0b4a8989bbc16af110561c929f836d8e23f4
christhudhas941 <merlindas941@gmail.com>
2025-03-13 15:44:20 +0530

    kjh

---
+++ dynamic/calculation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class calculation{
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int num1=scan.nextInt();
+        int num2=scan.nextInt();
+        int num3=num1+num2;
+        System.out.println(num3);
+
+    }
+}
