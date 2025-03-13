1235ee41b53af99a42c966fddd99b438f5c0d940
christhudhas941 <merlindas941@gmail.com>
2025-03-13 14:15:36 +0530

    loop

---
+++ loop/lo.java
@@ -0,0 +1,11 @@
+public class lo
+{
+    public static void main(String args[])
+    {
+        int i;
+        for (i = 0; i < 9; i++) {
+            
+         System.out.println(i);
+        }
+    }
+}
