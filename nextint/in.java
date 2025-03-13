324c67e7bd469476e15379e28954db0714292c44
christhudhas941 <merlindas941@gmail.com>
2025-03-13 15:15:09 +0530

    int

---
+++ nextint/in.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class in{
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int age =scan.nextInt();
+        System.out.println(age);
+    }
+}
