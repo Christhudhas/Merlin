e0c8bfa4ae25b8fe74a1beb612f2a1b6ca6736ed
christhudhas941 <merlindas941@gmail.com>
2025-03-13 10:58:23 +0530

    ifelse

---
+++ ie.java
@@ -0,0 +1,12 @@
+public class ie {
+    public static void main(String[] args) {
+        if (false) {
+        System.out.println("Their will be rain tommorrow");
+        }
+        else
+        {
+        System.out.println("enjoy sunlight");    
+        }
+    }
+    
+}
