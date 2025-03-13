fdf714b648642f7df73ca3a645b67edb5ffdf41b
Christhudhas <Merlindas941@gmail.com>
2025-03-14 02:21:08 +0530

    multidimensionarraY

---
+++ mdarray/md.java
@@ -0,0 +1,10 @@
+public class md
+{
+    public static void main(String args[])
+    {
+       
+        int a[][] = {{12},{24, 35, 98},{43}};
+         a[0][0] = 12;  
+         System.out.println(a[0][0]);  
+    }
+}
