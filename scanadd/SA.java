583980f4cb5df9ae51a8f262f944462f2e16217b
Christhudhas <Merlindas941@gmail.com>
2025-03-14 01:52:01 +0530

    scanneraddition

---
+++ scanadd/SA.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class SA{
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("num1:");
+      int x=sc.nextInt();
+      System.out.println("num2:");
+      int y=sc.nextInt();
+      int z=x+y;
+      System.out.println("total:"+z);
+    }
+   }
