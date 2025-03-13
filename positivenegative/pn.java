380f300654d5a8ff4af2a78001b34b7758d9c8e9
christhudhas941 <merlindas941@gmail.com>
2025-03-13 11:45:37 +0530

    pn

---
+++ positivenegative/pn.java
@@ -0,0 +1,13 @@
+public class pn {
+    public static void main(String args[])
+    {
+        int no=8;
+        if(no>=0){
+            System.out.println("It is positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+    
+}
---
+++ positivenegative/pn1.java
@@ -0,0 +1,15 @@
+public class pn1 
+  { 
+    public static void main(String args[])
+    {
+        int no=8;
+        if(no>=0)
+        {
+            System.out.println("It is positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+    
+}
