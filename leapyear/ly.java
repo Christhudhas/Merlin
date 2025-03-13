e55f56702d4249664de4a403c2123a3a03e810bc
Christhudhas <Merlindas941@gmail.com>
2025-03-14 01:34:18 +0530

    leapyear

---
+++ leapyear/ly.java
@@ -0,0 +1,14 @@
+class ly
+{
+    public static void main(String[] args) {
+        int i=2020;
+        if(i%4==0)
+        {
+            System.out.println("Leap Year");
+        }
+        else{
+            System.out.println("not a leap year");
+        }
+    }
+
+}
