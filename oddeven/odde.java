4d062a1dbc78b937ba8f18367287927d3ae6097a
christhudhas941 <merlindas941@gmail.com>
2025-03-13 14:24:25 +0530

    oddeven

---
+++ oddeven/odde.java
@@ -0,0 +1,13 @@
+public class odde {
+     public static void main(String args[])
+     {
+        int num =8;
+        if(num>=8)
+        {
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+     }    
+}
