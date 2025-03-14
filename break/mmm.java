a2a61f83182d925f06cd51b6a571e47a50e1ff9d
christhudhas941 <merlindas941@gmail.com>
2025-03-14 14:16:10 +0530

    mer

---
+++ break/mmm.java
@@ -0,0 +1,17 @@
+public class mmm{
+public static void main(String args[])
+{
+    int i=0;
+    while(i<10)
+    {
+        if (i%1==0) 
+        {
+        if(i>5){
+            break;
+        }    
+        System.out.println(i);
+        }
+        i=i+1;
+    }
+}
+}
