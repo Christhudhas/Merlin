7a6d4e9dfa1f4e6475b2161000666bbf7f384664
christhudhas941 <merlindas941@gmail.com>
2025-03-14 11:53:43 +0530

    split

---
+++ split/spl.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class spl{
+    public static void main(String args[])
+{
+   String num="hello,world";
+   String names[]=num.split(",");
+   System.out.println(names[2]);
+}
+}
