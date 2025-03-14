89d31f9261a7139db5a9e1d75e0891d23001739b
christhudhas941 <merlindas941@gmail.com>
2025-03-14 12:03:09 +0530

    german

---
+++ upperlower/len.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class len{
+    public static void main(String array[])
+    {
+        String name="merlin";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        
+    }
+}
+
