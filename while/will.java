d5de08bf58fb2ada40c399f37956b1256b9f6c84
christhudhas941 <merlindas941@gmail.com>
2025-03-14 10:13:20 +0530

    while

---
+++ while/will.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class will {
+    public static void main(String args[]) {
+        int i = 92;
+        while (i < 98) {
+            System.out.println(i);
+            i++; 
+        }
+    }
+}
