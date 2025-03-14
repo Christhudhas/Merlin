48a07eff5aebd55ceb808dd04d6549c022e5f53c
christhudhas941 <merlindas941@gmail.com>
2025-03-14 10:30:36 +0530

    find n numbers

---
+++ nnumbers/find.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class find {
+    public static void main(String args[])
+    {
+     Scanner scan=new Scanner(System.in);
+     System.out.println("enter a number:");
+     int n=scan.nextInt();
+     for (int i=1;i<=n;i++)
+     {
+        System.out.println(i);
+     }
+     scan.close();
+    }
+}
