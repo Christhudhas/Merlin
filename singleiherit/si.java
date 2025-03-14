2fb048e1eb8b2593f8880f300f21c609d151d01e
christhudhas941 <merlindas941@gmail.com>
2025-03-14 13:44:02 +0530

    m

---
+++ singleiherit/si.java
@@ -0,0 +1,20 @@
+import java.util.*
+class parent
+    {
+         System.out.println();
+
+    }
+    class Child extends parent{
+        void display()
+        {
+            add();
+
+        }
+    }
+    public class si{
+        public static void main (String args[])
+        {
+            Child obj=new Child();
+            obj.display();
+        }
+    }
