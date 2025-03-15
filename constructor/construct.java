ec93b3894b925a17744ee7a2a6f1411aa4c77d39
christhudhas941 <merlindas941@gmail.com>
2025-03-15 11:04:31 +0530

    java

---
+++ constructor/construct.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class construct {
+    
+    construct() {
+        String name = "merlin";
+        System.out.println(name);
+    }
+    
+    public static void main(String[] args) {
+        new construct();
+    }
+}
